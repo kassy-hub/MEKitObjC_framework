64b669ac2c8dfa06c17f08b5d9b8d41c3c077a13
Masahiro Kashima <kashima@seesaa.co.jp>
2018-05-01 21:00:22 +0900

    Recreate scheme, target for framework

---
+++ MEKitObjCFramework/MEKitObjCFramework.h
@@ -0,0 +1,19 @@
+//
+//  MEKitObjCFramework.h
+//  MEKitObjCFramework
+//
+//  Created by Masahiro Kashima on 2018/05/01.
+//  Copyright © 2018年 Mitsuharu Emoto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MEKitObjCFramework.
+FOUNDATION_EXPORT double MEKitObjCFrameworkVersionNumber;
+
+//! Project version string for MEKitObjCFramework.
+FOUNDATION_EXPORT const unsigned char MEKitObjCFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MEKitObjCFramework/PublicHeader.h>
+
+
